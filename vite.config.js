5e1df3e4101094cfb44a0da5d74a5a17a2909e85
Evan Raskob <evan@flkr.com>
2025-01-02 17:05:43 +0000

    again

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,16 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "./lib/main.js"),
+      entry: resolve(__dirname, "lib/main.js"),
       formats: ['es', 'umd'],
       name: "liveprinter",
-      fileName: (format) => `liveprinter.${format}.js`
+      fileName: "liveprinter"
     },
 
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["tonal", "liveprinter-utils"],
+      //external: ["tonal", "liveprinter-utils"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
