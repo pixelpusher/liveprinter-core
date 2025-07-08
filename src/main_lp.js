880890306d11c3af0a5d0342068e691f7ed67812
Evan Raskob <evan@flkr.com>
2025-07-08 21:01:14 +0100

    updates to testing

--- src/main_lp.js
+++ src/main_lp.js
@@ -1,43 +1,59 @@
 import { LivePrinter } from "../lib/main";
+import './main.css';
 
+const output = document.getElementById('output');
+let index = 1;
+
+function log(msg) {
+    output.innerHTML += `<span class='log'>${msg}</span><br>\n`;
+    console.log(msg);
+}
+
+function info(msg) {
+    output.innerHTML += `<span class='index'>${index++}:</span>&nbsp;<span class='info'>${msg}</span><br>\n`;
+    console.info(msg);
+}
 
 async function init() {
+
     const lp = new LivePrinter("UM2plus");
-    console.info(`BPM 140: ${lp.bpm(140)}`);
-    console.info(`travel speed A3: ${lp.tsp("A3")}`);
-    
-    console.info(`position: ${lp.x},${lp.y},${lp.z}`);
+    info (`BPM 140: ${lp.bpm(140)}`);
+    info(`travel speed A3: ${lp.tsp("A3")}`);
+
+    info(`travel speed A#3: ${lp.tsp("A#3")}`);
+
+    info(`position: ${lp.x},${lp.y},${lp.z}\n`);
     await lp.mov2({x:40, y:4, z:0.18, speed:40});   
-    console.info(`new position: ${lp.x},${lp.y},${lp.z}`);
+    info(`new position: ${lp.x},${lp.y},${lp.z}`);
 
-    console.info(`speed of a5: ${lp.midi2speed('a5')}`);
+    info(`speed of a5: ${lp.midi2speed('a5')}`);
 
-    console.info(`extrude at A5`);
+    info(`extrude at A5`);
     await lp.ext2({x:50, y:30, z:0.18, speed:'a5'});
-    console.log(`new position: ${lp.x}, ${lp.y}, ${lp.z}`);
+    log(`new position: ${lp.x}, ${lp.y}, ${lp.z}`);
 
-    console.log(`speed: ${lp.psp()} (not set by extrude!)`);
+    log(`speed: ${lp.psp()} (not set by extrude!)`);
     lp.bpm(120);
-    console.log(`speed: ${lp.psp('a5')} (a5 at 120bpm)`);
+    log(`speed: ${lp.psp('a5')} (a5 at 120bpm)`);
 
-    console.log(`extrude at sound of 200hz`);
-    console.log(`speed: ${lp.psp('200hz')}`);
+    log(`extrude at sound of 200hz`);
+    log(`speed: ${lp.psp('200hz')}`);
 
 
-    console.info(`Dimension or time 2b ${lp.parseAsDimensionOrTime('2b')}`);
-    console.info(`Dimension or time 1.5b ${lp.parseAsDimensionOrTime('1.5b')}`);
-    console.info(`Dimension or time 1 1/2b ${lp.parseAsDimensionOrTime('1 1/2b')}`);
+    info(`Dimension or time 2b ${lp.parseAsDimensionOrTime('2b')}`);
+    info(`Dimension or time 1.5b ${lp.parseAsDimensionOrTime('1.5b')}`);
+    info(`Dimension or time 1 1/2b ${lp.parseAsDimensionOrTime('1 1/2b')}`);
 
-    console.info(`Dimension or time 30mm ${lp.parseAsDimensionOrTime('30mm')}`);
-    console.info(`Dimension or time 30cm ${lp.parseAsDimensionOrTime('30cm')}`);
-    console.info(`Dimension or time 30 ${lp.parseAsDimensionOrTime('30')}`);
+    info(`Dimension or time 30mm ${lp.parseAsDimensionOrTime('30mm')}`);
+    info(`Dimension or time 30cm ${lp.parseAsDimensionOrTime('30cm')}`);
+    info(`Dimension or time 30 ${lp.parseAsDimensionOrTime('30')}`);
     
 
-    console.info(`extrude at 1 beats at 140bpm`);
+    info(`extrude at 1 beats at 140bpm`);
     lp.bpm(140);
     await lp.ext({x:'1b', speed:'440hz'});
-    console.log(`new position: ${lp.x}, ${lp.y}, ${lp.z}`);
-    console.log(`speed: ${lp.psp()}`);
+    log(`new position: ${lp.x}, ${lp.y}, ${lp.z}`);
+    log(`speed: ${lp.psp()}`);
 
     
     // test delay
